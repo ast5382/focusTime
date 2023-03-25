193ba340802c5a5445f3d0313bc322194f7b8e39
ast5382 <ast5382@rit.edu>
2023-03-25 11:22:24 -0400

    Added hour display on timer, and user input for seconds and hours.

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const startButton = document.getElementById("btnStart");
 const pauseButton = document.getElementById("btnPause");
 const ding = new Audio('sounds/ding.mp3');
 
-const hour = document.getElementById("hhInput");
-let hourVal = 99;
+const hour = document.getElementById("inputHour");
+const min = document.getElementById("inputMin");
+const sec = document.getElementById("inputSec");
+
+let hourVal ="";
+let minVal ="";
+let secVal ="";
+
 let resume = false;
 let timeLeft;
 
@@ -16,7 +22,17 @@ let timeInSec;
 
 //when user input entered, set value
 hour.addEventListener("input", () => {
-    hourVal = document.getElementById("hhInput").value
+    hourVal = document.getElementById("inputHour").value
+    resume=false;
+});
+//for minutes:
+min.addEventListener("input", () => {
+    minVal = document.getElementById("inputMin").value
+    resume=false;
+});
+//for seconds:
+sec.addEventListener("input", () => {
+    secVal = document.getElementById("inputSec").value
     resume=false;
 });
 
@@ -28,25 +44,32 @@ pauseButton.style.visibility = 'hidden';
 //interval calls updateTimer every second
 //Possibly unnecessary function
 function setTimer() {
-    // time set by user
-    const setTime = hourVal;
-    timeInSec = setTime * 60;
+
+    // format time set by user
+  // const setTime = (hourVal*3600) + (minVal*60) + secVal;
+    timeInSec = formatTime(hourVal, minVal, secVal);
 
     countInterval = setInterval(updateTimer, 1000)
     toggleButtonView(startButton, pauseButton);
 
     //testing:
-    console.log("hour: " + hourVal);
+    console.log("minutes: " + minVal);
     console.log("setTime: " + setTime);
 }
 
 function startTimer() {
+    //testing
+    console.log(hourVal);
+    console.log(minVal);
+    console.log(secVal);
+    console.log(timeInSec)
+
     if (resume) {
         timeInSec = timeLeft;
         countInterval = setInterval(updateTimer, 1000)
     } else {
-        const setTime = hourVal;
-        timeInSec = setTime * 60;
+        const setTime = minVal;
+        timeInSec = formatTime(hourVal, minVal, secVal);
         countInterval = setInterval(updateTimer, 1000)
     }
     toggleButtonView(startButton, pauseButton);
@@ -54,12 +77,17 @@ function startTimer() {
 
 //decreses the time shown by 1 second and stops when time hits 0
 function updateTimer() {
+    let hours = 0;
     let mins = Math.floor(timeInSec / 60);
     let secs = timeInSec % 60;
-
-    displayTime(mins, secs);
+    if(mins>60){
+        hours = Math.floor(timeInSec/3600);
+        mins = Math.floor(60*(timeInSec%3600)/3600);
+    }
+    console.log(`${hours},${mins},${secs}`)
+    displayTime(hours, mins, secs);
     timeInSec--;
-    if (mins <= 0 && secs <= 0) {
+    if (hour<=0 && mins <= 0 && secs <= 0) {
         clearInterval(countInterval);
         console.log("timer ended");
         playSound();
@@ -67,11 +95,24 @@ function updateTimer() {
 }
 
 //creates string format of time
-function displayTime(min, sec) {
+function displayTime(hours, min, sec) {
     sec = sec < 10 ? '0' + sec : sec;
     min = min < 10 ? '0' + min : min;
-    timer.innerHTML = `${min}:${sec}`;
+    
+    if(hours>=1){
+        // hours = hours < 10 ? '0' + hours : hours;
+        timer.innerHTML = `${hours}:${min}:${sec}`;
+    }
+    else{
+        timer.innerHTML = `${min}:${sec}`;
+    }
+    
+    
 
+}
+
+function formatTime(h,m,s){
+    return (h*3600) + (m*60) + s;
 }
 
 //is this necessary?
