f30f6ef344215d2e8a73b46d2d296aaa41f0ef68
ast5382 <ast5382@rit.edu>
2023-03-27 14:11:47 -0400

    Added break timer function and button. Need refactoring for pauseButton to work with break function.

--- script.js
+++ script.js
@@ -1,53 +1,61 @@
+//HTML element initializations
 const timer = document.getElementById("timer");
 const startButton = document.getElementById("btnStart");
 const pauseButton = document.getElementById("btnPause");
 const restartButton = document.getElementById("btnRestart");
+const breakButton = document.getElementById("btnBreak");
 const input = document.querySelectorAll("input");
 const container = document.getElementsByClassName("container");
 const ding = new Audio('sounds/ding.mp3');
 
+//For focus timer
 const hour = document.getElementById("inputHour");
 const min = document.getElementById("inputMin");
 const sec = document.getElementById("inputSec");
-
 let hourVal = "";
 let minVal = "";
 let secVal = "";
 
+//For break timer
+let breakHour = "";
+let breakMin = 5;
+let breakSec = "";
+
+//Mutable variable for interval ID access
+let countInterval;
+
+//Mutable variables for global access
+let timeInSec;
 let resume = false;
 let timeLeft;
 
-//mutable variable for interval ID access
-let countInterval;
 
-//mutable variable for global access
-let timeInSec;
-
-//when user input entered, set value
+//When user input entered, set value
 hour.addEventListener("input", () => {
     hourVal = document.getElementById("inputHour").value
     resume = false;
 });
-//for minutes:
+//For minutes:
 min.addEventListener("input", () => {
     minVal = document.getElementById("inputMin").value
     resume = false;
 });
-//for seconds:
+//For seconds:
 sec.addEventListener("input", () => {
     secVal = document.getElementById("inputSec").value
     resume = false;
 });
 
-//start & pause timer on start button click
+//Start & pause timer on start button click
 startButton.addEventListener("click", startTimer);
 pauseButton.addEventListener("click", pauseTimer);
-//do this in css
+//*do this in css
 pauseButton.style.visibility = 'hidden';
 
 restartButton.addEventListener("click", restartTimer);
+breakButton.addEventListener("click", breakTimer);
 
-// container.style.visibility='hidden';
+
 timer.addEventListener("click", () => {
     // console.log("clicked");
     container[0].style.visibility = 'visible';
@@ -55,11 +63,10 @@ timer.addEventListener("click", () => {
 });
 
 
-//hides container if click was made outside timer or container
+//Hides container if click was made outside timer or container
 const onClickOutside = (event) => {
     const withinBoundary1 = event.composedPath().includes(timer);
     const withinBoundary2 = event.composedPath().includes(container[0]);
-
 
     if (withinBoundary1) {
         // console.log("Click happened inside element")
@@ -76,12 +83,14 @@ const onClickOutside = (event) => {
 // document.addEventListener('click', (event) => {
 //     document.removeEventListener})
 
-//change color on hover over textinput
+//Change color of text input on hover over
 input.forEach((i) => {
     i.addEventListener("mouseover", () => { i.style.backgroundColor = "#F5F5F5" });
     i.addEventListener("mouseout", () => { i.style.backgroundColor = "white" });
 })
 
+
+// Starts a timer
 function startTimer() {
     if (resume) {
         timeInSec = timeLeft;
@@ -96,7 +105,7 @@ function startTimer() {
     restartButton.style.visibility = 'hidden';
 }
 
-//decreses the time shown by 1 second and stops when time hits 0
+//Decreses the time shown by 1 second and stops when time hits 0
 function updateTimer() {
     let hours = 0;
     let mins = Math.floor(timeInSec / 60);
@@ -113,12 +122,14 @@ function updateTimer() {
         clearInterval(countInterval);
         console.log("timer ended");
         playSound();
+        
+        breakButton.style.visibility='visible';
     }
 
 
 }
 
-//creates string format of time
+//Creates string format of time
 function displayTime(hours, min, sec) {
     sec = sec < 10 ? '0' + sec : sec;
     min = min < 10 ? '0' + min : min;
@@ -132,17 +143,17 @@ function displayTime(hours, min, sec) {
     }
 }
 
+//Formats user inputted time into seconds to be compatible with updateTimer
 function formatTime(h, m, s) {
-
     return Number(h * 3600) + (Number(m * 60)) + Number(s);
 }
 
-//is this necessary?
+//*is this necessary?
 function playSound() {
     ding.play();
 }
 
-//pauses timer
+//Pauses timer, hides pause button and shows play & restart button
 function pauseTimer() {
     timeLeft = timeInSec;
     // console.log("time left: " + timeLeft);
@@ -152,12 +163,14 @@ function pauseTimer() {
     resume = true;
 }
 
-//toggle visibility status of buttons
+//Toggle visibility status of buttons
+//*replace with showElement() and hideElement() functions?
 function toggleButtonView(btnHide, btnShow) {
     btnHide.style.visibility = 'hidden';
     btnShow.style.visibility = 'visible';
 }
 
+//Resets and starts the timer to original time
 function restartTimer(){
     pauseTimer();
     resume = false
@@ -165,3 +178,9 @@ function restartTimer(){
 }
 
 
+function breakTimer(){
+
+    timeInSec = formatTime(breakHour, breakMin, breakSec);
+    countInterval = setInterval(updateTimer, 1000)
+    toggleButtonView(breakButton, pauseButton);
+}
