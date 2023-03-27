f0b7e3d284bf1a147341aa2f818650886db118b2
ast5382 <ast5382@rit.edu>
2023-03-27 13:06:33 -0400

    Added timer restart button & functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const timer = document.getElementById("timer");
 const startButton = document.getElementById("btnStart");
 const pauseButton = document.getElementById("btnPause");
+const restartButton = document.getElementById("btnRestart");
 const input = document.querySelectorAll("input");
 const container = document.getElementsByClassName("container");
 const ding = new Audio('sounds/ding.mp3');
@@ -9,9 +10,9 @@ const hour = document.getElementById("inputHour");
 const min = document.getElementById("inputMin");
 const sec = document.getElementById("inputSec");
 
-let hourVal ="";
-let minVal ="";
-let secVal ="";
+let hourVal = "";
+let minVal = "";
+let secVal = "";
 
 let resume = false;
 let timeLeft;
@@ -25,58 +26,60 @@ let timeInSec;
 //when user input entered, set value
 hour.addEventListener("input", () => {
     hourVal = document.getElementById("inputHour").value
-    resume=false;
+    resume = false;
 });
 //for minutes:
 min.addEventListener("input", () => {
     minVal = document.getElementById("inputMin").value
-    resume=false;
+    resume = false;
 });
 //for seconds:
 sec.addEventListener("input", () => {
     secVal = document.getElementById("inputSec").value
-    resume=false;
+    resume = false;
 });
 
 //start & pause timer on start button click
 startButton.addEventListener("click", startTimer);
 pauseButton.addEventListener("click", pauseTimer);
+//do this in css
 pauseButton.style.visibility = 'hidden';
 
+restartButton.addEventListener("click", restartTimer);
+
 // container.style.visibility='hidden';
-timer.addEventListener("click", ()=>{
-    console.log("clicked");
-    container[0].style.visibility='visible';
-    //hideOnOutsideClick(timer, container[0]);
+timer.addEventListener("click", () => {
+    // console.log("clicked");
+    container[0].style.visibility = 'visible';
     window.addEventListener("click", onClickOutside);
-} );
+});
 
 
 //hides container if click was made outside timer or container
-const onClickOutside = (event)=>{
+const onClickOutside = (event) => {
     const withinBoundary1 = event.composedPath().includes(timer);
     const withinBoundary2 = event.composedPath().includes(container[0]);
 
 
-        if (withinBoundary1) {
-        console.log("Click happened inside element")
-        } else if(withinBoundary2){
-            console.log("Click happened inside element")
-        } else{
-            container[0].style.visibility='hidden';
-        console.log("Click happened **OUTSIDE** element");
+    if (withinBoundary1) {
+        // console.log("Click happened inside element")
+    } else if (withinBoundary2) {
+        // console.log("Click happened inside element")
+    } else {
+        container[0].style.visibility = 'hidden';
+        // console.log("Click happened **OUTSIDE** element");
         window.removeEventListener("click", onClickOutside);
-        }
     }
+}
 
-    
-    // document.addEventListener('click', (event) => {
-    //     document.removeEventListener})
+
+// document.addEventListener('click', (event) => {
+//     document.removeEventListener})
 
 //change color on hover over textinput
-input.forEach((i)=>{
-    i.addEventListener("mouseover", ()=>{i.style.backgroundColor ="#F5F5F5"});
-    i.addEventListener("mouseout", ()=>{i.style.backgroundColor="white"});
+input.forEach((i) => {
+    i.addEventListener("mouseover", () => { i.style.backgroundColor = "#F5F5F5" });
+    i.addEventListener("mouseout", () => { i.style.backgroundColor = "white" });
 })
 
 function startTimer() {
@@ -90,6 +93,7 @@ function startTimer() {
         countInterval = setInterval(updateTimer, 1000)
     }
     toggleButtonView(startButton, pauseButton);
+    restartButton.style.visibility = 'hidden';
 }
 
 //decreses the time shown by 1 second and stops when time hits 0
@@ -97,42 +101,40 @@ function updateTimer() {
     let hours = 0;
     let mins = Math.floor(timeInSec / 60);
     let secs = timeInSec % 60;
-    if(mins>60){
-        hours = Math.floor(timeInSec/3600);
-        mins = Math.floor(60*(timeInSec%3600)/3600);
+    if (mins > 60) {
+        hours = Math.floor(timeInSec / 3600);
+        mins = Math.floor(60 * (timeInSec % 3600) / 3600);
     }
-   
+
     displayTime(hours, mins, secs);
     timeInSec--;
-    if (hour<=0 && mins <= 0 && secs <= 0) {
+
+    if (hours <= 0 && mins <= 0 && secs <= 0) {
         clearInterval(countInterval);
         console.log("timer ended");
         playSound();
     }
-    
-   
+
+
 }
 
 //creates string format of time
 function displayTime(hours, min, sec) {
     sec = sec < 10 ? '0' + sec : sec;
     min = min < 10 ? '0' + min : min;
-    
-    if(hours>=1){
+
+    if (hours >= 1) {
         // hours = hours < 10 ? '0' + hours : hours;
         timer.innerHTML = `${hours}:${min}:${sec}`;
     }
-    else{
+    else {
         timer.innerHTML = `${min}:${sec}`;
     }
-    
-    
-
 }
 
-function formatTime(h,m,s){
+function formatTime(h, m, s) {
 
-    return Number(h*3600) + (Number(m*60)) + Number(s);
+    return Number(h * 3600) + (Number(m * 60)) + Number(s);
 }
 
 //is this necessary?
@@ -146,6 +148,7 @@ function pauseTimer() {
     // console.log("time left: " + timeLeft);
     clearInterval(countInterval);
     toggleButtonView(pauseButton, startButton);
+    restartButton.style.visibility = 'visible';
     resume = true;
 }
 
@@ -155,6 +158,10 @@ function toggleButtonView(btnHide, btnShow) {
     btnShow.style.visibility = 'visible';
 }
 
-
+function restartTimer(){
+    pauseTimer();
+    resume = false
+    startTimer();
+}
 
 
